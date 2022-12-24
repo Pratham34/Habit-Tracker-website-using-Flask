e209be9f8095b4ab436c43ebf2380cc2b6db64b8
Pratham <bhardwajpratham7@gmail.com>
2022-12-24 11:19:58 +0530

    Adding files

---
+++ routes.py
@@ -0,0 +1,71 @@
+import datetime
+import uuid
+# from collections import defaultdict
+from flask import Blueprint,current_app,render_template,request,redirect,url_for
+
+pages = Blueprint("habits",__name__,template_folder="templates",static_folder="static")
+
+# habits = ["Test habit","Test habit 2"]
+# completions = defaultdict(list)
+
+@pages.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.datetime):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+        return dates
+    
+    # So now every jinja template will have access to a date_range variable and that variable will contain the date_range function that we have defined here.
+    return {"date_range":date_range}        
+
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year,today.month,today.day)    # by default time,min,sec are 0 !
+
+
+@pages.route("/")
+def index():
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.datetime.fromisoformat(date_str)
+    else:
+        selected_date = datetime.datetime.today()
+    
+    # habits_on_date is a list of dictionaries where each dictionary has the data(keys) - id , name and added !!
+    habits_on_date = current_app.db.habits.find({"added" : {"$lte" : selected_date}})
+
+    completions = [
+        habit["habit"]
+        for habit in current_app.db.completions.find({"date":selected_date})
+    ]
+
+    return render_template("index.html",
+    # habits=habits,
+    habits = habits_on_date,
+    # completions=completions[selected_date],
+    completions=completions,
+    title="Habit Tracker - Home",
+    selected_date=selected_date)
+
+
+@pages.route("/add",methods=["GET","POST"])
+def add_habit():
+    today = today_at_midnight()
+
+    if request.method == "POST":
+        # habits.append(request.form.get("habit"))
+
+        current_app.db.habits.insert_one(
+            {"_id": uuid.uuid4().hex,"added": today,"name": request.form.get("habit")}
+        )
+
+    return render_template("add_habit.html",title="Habit Tracker - Add Habit",selected_date=today)    
+
+@pages.route("/complete",methods=["POST"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitId")
+    date = datetime.datetime.fromisoformat(date_string)
+    # completions[date].append(habit)
+    current_app.db.completions.insert_one({"date":date,"habit":habit})
+
+    return redirect(url_for(".index",date=date_string))    
